1b52bdd6b7758cbfb7bd0663ad81528f3b222d2b
Michael Spencer Quinto <SpencerMAQ@users.noreply.github.com>
2021-07-24 22:50:18 +0800

    standalone working script to scrape etymology text ONLY

--- scraper/webfinder.py
+++ scraper/webfinder.py
@@ -1,12 +1,28 @@
 import urllib.request
 import urllib.parse
+from bs4 import BeautifulSoup
+import json
 
 term = '本'.encode('utf-8')
+# term = '夢'.encode('utf-8')
 
 site = 'https://www.dong-chinese.com/dictionary/{}'.format(urllib.parse.quote(term))
 site2 = 'https://jisho.org/search/{}'.format(urllib.parse.quote(term))
 site3 = 'http://www.weblio.jp/content/'.format(urllib.parse.quote(term))
-data = urllib.request.urlopen(site)
 
-for l in data.readlines():
-    print(l.decode('utf-8'))
+response = urllib.request.urlopen(site)
+soup = BeautifulSoup(response, features='html.parser')
+soup_text = str(soup)
+
+# get only the relevant JS part of dong-chinese which is formatted as a JSON
+soup_text = soup_text.split('<script>window["')[-1].split('__sink__charData_')[-1].split(']=')[-1]
+# returns a pure JSON object
+soup_text = soup_text.split(';</script>')[0]
+
+# turn into JSON and parse
+soup_json = json.loads(soup_text)
+etymology = soup_json['hint']
+decomposition = soup_json['components']
+
+print(decomposition)
+
