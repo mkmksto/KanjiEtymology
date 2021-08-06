a52fba46f21e5bf4736a9e15fe951860a0d842c9
Michael Spencer Quinto <SpencerMAQ@users.noreply.github.com>
2021-08-06 21:15:34 +0800

    consts.py file

---
+++ scraper/consts.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# Copyright: Tanaka Aiko (https://github.com/aiko-tanaka)
+# License: GNU AGPL, version 3 or later; https://www.gnu.org/licenses/agpl-3.0.en.html
+
+# text shown while processing cards
+LABEL_PROGRESS_UPDATE = 'Scraping Kanji Etymologies'
+# text shown on menu to run the functions
+LABEL_MENU = 'Extract Kanji from Vocab, and fetch etymologies into Kanji_Etym field'
